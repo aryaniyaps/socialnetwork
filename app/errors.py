af9480afee377fbfe4320e190faccd5373036cf1
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-07-17 21:12:29 +0530

    remove unnecessary pass statement

--- app/errors.py
+++ app/errors.py
@@ -1,12 +1,12 @@
 class BaseError(Exception):
     """Base error class."""
+
     def __init__(self, message: str) -> None:
         self.message = message
 
 
 class ResourceNotFound(BaseError):
     """
-    Indicate that the requested 
+    Indicate that the requested
     resource doesn't exist.
     """
-    pass
